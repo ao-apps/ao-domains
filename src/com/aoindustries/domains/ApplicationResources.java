fddce60fd32cdb8525451f649c3e1747aa4ad515
Dan Armstrong <dan@aoindustries.com>
2011-08-26 02:50:57 +0000

    New aocode-public JAR.

--- src/com/aoindustries/domains/ApplicationResources.java
+++ src/com/aoindustries/domains/ApplicationResources.java
@@ -33,9 +33,9 @@ public final class ApplicationResources extends EditableResourceBundle {
      */
     public ApplicationResources() {
         super(
-            new File(System.getProperty("user.home")+"/common/ao/cvswork/ao-domains/src/com/aoindustries/domains/ApplicationResources.properties"),
             Locale.ROOT,
-            bundleSet
+            bundleSet,
+            new File(System.getProperty("user.home")+"/common/ao/cvswork/ao-domains/src/com/aoindustries/domains/ApplicationResources.properties")
         );
     }
 
