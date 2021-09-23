72b216ddbf21e58361bf792994e361a3be25ec14
AO Industries, Inc <support@aoindustries.com>
2021-09-22 20:29:43 -0500

    Catching SecurityException and logging warning
    Previously, a SecurityException during class initialization, such as when
    trying to access the "user.home" system property, would cause the
    ResourceBundle initialization to fail.  This failure was cached and any
    subsequent attempts to access the bundle, even if it would then succeeed,
    would fail.
    
    With this change, the resource bundle will still function, but without any
    direct access to source files.

--- src/main/java/com/aoindustries/domains/ApplicationResources.java
+++ src/main/java/com/aoindustries/domains/ApplicationResources.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2011, 2020 by AO Industries, Inc.,
+ * Copyright 2009-2011, 2020, 2021 by AO Industries, Inc.,
  * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
  * All rights reserved.
  */
@@ -11,6 +11,8 @@ import com.aoindustries.util.i18n.EditableResourceBundleSet;
 import java.io.File;
 import java.util.Arrays;
 import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author  AO Industries, Inc.
@@ -26,7 +28,16 @@ public final class ApplicationResources extends EditableResourceBundle {
     );
 
 	static File getSourceFile(String filename) {
-		return new File(System.getProperty("user.home") + "/maven2/ao/ao-domains/src/main/resources/com/aoindustries/domains", filename);
+		try {
+			return new File(System.getProperty("user.home") + "/maven2/ao/ao-domains/src/main/resources/com/aoindustries/domains", filename);
+		} catch(SecurityException e) {
+			Logger.getLogger(ApplicationResources.class.getName()).log(
+				Level.WARNING,
+				"Unable to locate source file: " + filename,
+				e
+			);
+			return null;
+		}
 	}
 
     public ApplicationResources() {
